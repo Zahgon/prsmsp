1c1fabe4e0be954d7c4b4f45dfdabc216750ccd4
parsariyahi <pany.parsariyahi@gmail.com>
2024-07-11 11:26:51 +0330

    feature(abc): add bulk sending sms

--- prsmsp/abstracts/abcpanel.py
+++ prsmsp/abstracts/abcpanel.py
@@ -11,3 +11,7 @@ class ABCSmsPanel(abc.ABC):
     @abc.abstractmethod
     def send_sms(self):
         raise NotImplementedError
+
+    @abc.abstractmethod
+    def send_sms_bulk(self):
+        raise NotImplementedError
